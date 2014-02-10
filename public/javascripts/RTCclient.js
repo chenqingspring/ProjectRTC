88e32761e619bd55d39a435fae582efe1cb0c2a7
CHEN Qing <qing.chen@rea-group.com>
2014-02-11 07:41:33 +0800

    add new stun server.

--- public/javascripts/RTCclient.js
+++ public/javascripts/RTCclient.js
@@ -99,6 +99,7 @@ function RTCclient () {
     url: window.location.origin,
     peerConnectionConfig: {
       iceServers: [{"url": "stun:23.21.150.121"}
+                  ,{"url": "stun:176.58.114.68:3478"}
                   ,{"url": "stun:stun.l.google.com:19302"}]
     },
     peerConnectionConstraints: {
