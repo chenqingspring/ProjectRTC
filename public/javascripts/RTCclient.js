3239604c386f3e9890ca1c3b2a6fb617e7974e7b
CHEN Qing <qing.chen@rea-group.com>
2014-02-11 14:03:21 +0800

    Revert "remove other stun servers."
    This reverts commit 5b1368c6f6f5c8c5d92e4682f3548a31afbd42e4.

--- public/javascripts/RTCclient.js
+++ public/javascripts/RTCclient.js
@@ -98,7 +98,9 @@ function RTCclient () {
   this.config = {
     url: window.location.origin,
     peerConnectionConfig: {
-      iceServers: [{"url": "stun:176.58.114.68:3478"}]
+      iceServers: [{"url": "stun:23.21.150.121"}
+                  ,{"url": "stun:176.58.114.68:3478"}
+                  ,{"url": "stun:stun.l.google.com:19302"}]
     },
     peerConnectionConstraints: {
       optional: [{"DtlsSrtpKeyAgreement": true}]
